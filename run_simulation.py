a55bfe64b4c40c6ee82af091f02f407ee6cef8da
buzinguyen <buzinguyen@gmail.com>
2023-02-28 02:57:47 -0500

    working new model of single unit cell of miura bot

--- run_simulation.py
+++ run_simulation.py
@@ -14,21 +14,20 @@ p.setGravity(0, 0, -9.81)
 p.loadURDF("plane.urdf", [0, 0, 0], [0, 0, 0, 1])
 
 # load parallelogram bot urdf
-start_pos = [0, 0, 0.01]
+start_pos = [0, 0, 0.2]
 start_orientation = p.getQuaternionFromEuler([0., 0., 0.])
 urdf = "miura_bot.urdf"
 botId = p.loadURDF(urdf, start_pos, start_orientation, globalScaling=0.01)
 
-for i in range(p.getNumJoints(botId)):
-  print(p.getJointInfo(botId, i))
+# for i in range(p.getNumJoints(botId)):
+#   print(p.getJointInfo(botId, i))
 
 # set up wheel sliders
-targetVelocitySlider = p.addUserDebugParameter("targetVelocity1", -100, 100, 0)
-targetVelocitySlider2 = p.addUserDebugParameter("targetVelocity2", -100, 100, 0)
+targetVelocitySlider = p.addUserDebugParameter("targetVelocity1", -2.0, 2.0, 0)
+targetVelocitySlider2 = p.addUserDebugParameter("targetVelocity2", -2.0, 2.0, 0)
 
 # run simulation
 while True:
-    
     # set wheel velocities
     targetVelocity = p.readUserDebugParameter(targetVelocitySlider)
     p.setJointMotorControlArray(bodyIndex=botId, 
@@ -43,4 +42,4 @@ while True:
 
     # step
     p.stepSimulation()
-    # time.sleep(1./240.)
+    time.sleep(1./240.)
